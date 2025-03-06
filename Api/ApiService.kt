05a5549e3009df8fd7d178f66e71bbae70ea5a59
Bastien <baspicco@gmail.com>
2025-03-06 17:09:05 +0100

    Add files via upload

--- Api/ApiService.kt
+++ Api/ApiService.kt
@@ -1,1 +1,12 @@
+package fr.isen.bastien.isensmartcompanion.Api
 
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Query
+import android.app.usage.UsageEvents
+import android.telecom.Call
+
+interface ApiService{
+    @GET("events.json")
+    fun getEvents(): Call<List<UsageEvents.Event>>
+}
